58630cb5d74c4fcde0fd576d6f87fba4a57cdc32
Nicolas Sauzede <nsauzede@laposte.net>
2021-04-09 13:02:14 +0200

    Reverse string

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-// this is a simple interface without leveraging from the Rust language bells and whistles
-#[no_mangle]
-pub extern fn add(x: u32, y: u32) -> u32 {
-    x + y
-}
-#[no_mangle]
-pub extern fn sub(x: u32, y: u32) -> u32 {
-    x - y
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+// Reverse a string coming from JS 
+#[wasm_bindgen]
+pub fn reverse(s: String) -> String {
+    s.chars().rev().collect::<String>()
 }
