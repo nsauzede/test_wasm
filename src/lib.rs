e3bf706a47d2430c84754acceb29696cd38499f6
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2024-12-10 11:13:44 +0100

    Restore add/sub

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
+
+#[no_mangle]
+pub extern fn add(x: u32, y: u32) -> u32 {
+    x + y
+}
+
+#[no_mangle]
+pub extern fn sub(x: u32, y: u32) -> u32 {
+    x - y
+}
+
 // Reverse a string coming from JS 
 #[wasm_bindgen]
 pub fn reverse(s: String) -> String {
